4e14ad65f79223e0b33f8927d4a383e9f3e8fd87
Jordan Rogers <jrdnrgrs@gmail.com>
2023-03-23 09:50:50 -0500

    updated audio files

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 // Initialize .env config file
 require('dotenv').config();
 const sessionID = process.env.SESSION_ID;
-const { config, createAudioFromText } = require('./index')
+const BASE_URL = process.env.BASE_URL;
+const { config, createAudioFromText } = require('tiktok-tts')
 
-config(sessionID);
+config(sessionID, BASE_URL);
+//config(sessionID);
 
 async function yourFunction(){
     await createAudioFromText("it's me rocket the raccoon! it's me rocket the raccoon. it's me ROCKET the raccoon...","audio","en_us_rocket");
